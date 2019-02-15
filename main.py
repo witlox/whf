cfd3fd8b277650abd95cf9fc41a3481a9895c770
Pim Witlox <pim@witlox.io>
2019-02-15 13:41:39 +0100

    added logging config

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import argparse
+from logging.config import dictConfig
 
 from fabric import Connection
 from flask import Flask, json, request
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 
 app = Flask(__name__)
 
